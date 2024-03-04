721e707732d0639d3b3ac13483809cbee838f3ae
adamanta1 <elena.brashkina2020@gmail.com>
2024-03-05 01:17:27 +0400

    feat: solve 3 more tasks

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -97,8 +97,9 @@ function compareObjects(obj1, obj2) {
  *    isEmptyObject({}) => true
  *    isEmptyObject({a: 1}) => false
  */
-function isEmptyObject(/* obj */) {
-  throw new Error('Not implemented');
+function isEmptyObject(obj) {
+  const arr = Object.keys(obj);
+  return arr.length === 0;
 }
 
 /**
@@ -117,8 +118,8 @@ function isEmptyObject(/* obj */) {
  *    immutableObj.newProp = 'new';
  *    console.log(immutableObj) => {a: 1, b: 2}
  */
-function makeImmutable(/* obj */) {
-  throw new Error('Not implemented');
+function makeImmutable(obj) {
+  return Object.freeze(obj);
 }
 
 /**
@@ -131,8 +132,14 @@ function makeImmutable(/* obj */) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  const word = [];
+  Object.entries(lettersObject).forEach(([key, value]) => {
+    value.forEach((item) => {
+      word[item] = key;
+    });
+  });
+  return word.join('');
 }
 
 /**
